b2ef300f4eeb5d247eb266dbb80f7c0088c4a442
Sean <sgilmore@michael-sean.com>
2017-08-13 14:13:28 -0400

    Created seeds and schema files

---
+++ db/schema.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Use native promises
+mongoose.Promise = global.Promise;
+
+const TimeStampSchema = new Schema ({
+  created_at: Date,
+  updated_at: Date,
+});
+
+TimeStampSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
+});
+
+const CashOutFormSchema = new Schema({
+  charge_sales_plus_tips:       Number,
+  charge_tips:                  Number,
+  charge_sales_less_tips:       Number,
+  total_sales:                  Number,
+  total_charge_sales_plus_tips: Number,
+  total_pay_outs:               Number,
+  total_charges_plus_pay_outs:  Number,
+  total_projected_cash_on_hand: Number,
+  total_actual_cash_on_hand:    Number,
+  total_cash_amount_difference: Number,
+  total_gratuity_min_8_precent: Number,
+  digital_signature:            String,
+  time_stamp: [TimeStampSchema],
+});
+
+const UserSchema = new Schema({
+  user_name: String,
+  pass_code: { type: Number, required: true, unique: true },
+  admin: false,
+  cashOutforms: [CashOutFormSchema],
+  time_stamp: [TimeStampSchema]
+});
+
+const CashOutListSchema = new Schema({
+  user_name: [UserSchema.user_name],
+  title: [CashoutFormSchema.time_stamp]
+});
+
+const TimeStampModel = mongoose.model('TimeStamp', TimeStampSchema);
+const CashOutFormModel = mongoose.model('CashOut', CashOutFormSchema);
+const UserModel = mongoose.model('User', UserSchema);
+const CashOutListModel = mongoose.model('CashOutList', CashOutListSchema);
+
+module.exports = {
+  TimeStamp: TimeStampModel,
+  CashOutForm: CashOutFormModel,
+  User: UserModel,
+  CashOutList: CashOutListModel
+};
--- db/seeds.js
+++ db/seeds.js
@@ -1,3 +1,97 @@
 require("dotenv").config();
+
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
+
+const TimeStamp = require('../models/');
+const CashOutForm = require('../models/');
+const User = require('../models/');
+const CashOutList = require('../models/');
+
+// Use native promises
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+
+// Remove seeds
+
+TimeStamp.remove({}, function(error){
+  console.log(error);
+});
+
+CashOutForm.remove({}, function(error){
+  console.log(error);
+});
+
+User.remove({}, function(error){
+  console.log(error);
+});
+
+CashOutList.remove({}, function(error){
+  console.log(error);
+});
+
+//Seeded data
+
+const Gilly = new User({
+  user_name: 'Gilly',
+  pass_code: '1234',
+  admin: true,
+  cash_out_forms: ['00000','00001'],
+  time_stamp: '10011'
+});
+
+const Brian = new User ({
+  user_name: 'Brian',
+  pass_code: '4321',
+  admin: false,
+  cash_out_forms: [],
+  time_stamp: '11010'
+});
+
+const Sean = new User ({
+  user_name: 'Sean',
+  pass_code: '0019',
+  admin: false,
+  cash_out_forms: ['00002']
+});
+
+// Save seeds
+
+Gilly.save( (error)=>{
+  if (error) console.log('Gilly user' + error);
+  console.log('Gilly created');
+});
+
+Brian.save( (error)=>{
+  if (error) console.log('Brian user' + error);
+  console.log('Brian created');
+});
+
+Sean.save( (error)=>{
+  if (error) console.log('Sean user' + error);
+  console.log('Sean created');
+});
+
+// CONNECTION EVENTS
+db.once('open', function() {
+  console.log("Opened mongoose.");
+});
+
+db.once('close', function() {
+  console.log("Closed mongoose.");
+});
+
+db.on('connected', function() {
+  console.log('Mongoose connected to ' + db.host + ':' + db.port + '/' + db.name);
+});
+
+db.on('error', function(err) {
+  console.log('Mongoose connection error: ' + err);
+});
+
+db.on('disconnected', function() {
+  console.log('Mongoose disconnected');
+});
+
+module.exports = db;
